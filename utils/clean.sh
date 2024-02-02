0c0cf483e86e9c2a53578117aeba697ee3857a35
Davide Barbagallo <barbang.davide@gmail.com>
2024-02-02 18:34:55 +0100

    feat: add cleanup script

---
+++ utils/clean.sh
@@ -0,0 +1,10 @@
+docker rm italian-covid-stats-kafka-topic-1 
+docker rm italian-covid-stats-kafka-server-1 
+docker rm italian-covid-stats-kafka-ui-1
+docker rm italian-covid-stats-python_server-1 
+docker rm italian-covid-stats-logstash-1 
+docker image rm italian-covid-stats-kafka-server:latest 
+docker image rm italian-covid-stats-kafka-ui:latest 
+docker image rm italian-covid-stats-kafka-topic:latest 
+docker image rm italian-covid-stats-logstash:latest 
+docker image rm italian-covid-stats-python_server:latest
