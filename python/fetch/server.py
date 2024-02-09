e07e7e6b9354b8a395c4542213e3caae6505212e
Davide Barbagallo <barbang.davide@gmail.com>
2024-02-10 00:07:16 +0100

    feat: remove arrow for datetime

--- python/fetch/server.py
+++ python/fetch/server.py
@@ -6,6 +6,8 @@ import os
 import requests
 import time
 
+from datetime import datetime, timedelta
+
 dotenv.load_dotenv()
 
 time.sleep(float(os.getenv("WARMUP_SECONDS")))
@@ -20,20 +22,22 @@ handler = logstash_async.handler.AsynchronousLogstashHandler(
 )
 logger.addHandler(handler)
 
-start_date = arrow.Arrow(year=2020, month=2, day=24)
-test_date = arrow.utcnow().shift(weeks=-2)
-end_date = (
-    arrow.utcnow().shift(weekday=4, weeks=-1)
-    if arrow.utcnow().weekday() >= 4
-    else arrow.utcnow().shift(weeks=-2, weekday=4)
-)
+start_date = datetime(year=2020, month=2, day=24)
+test_date = datetime.utcnow() - timedelta(weeks=4)
+
+current_date = datetime.utcnow()
+if current_date.weekday() >= 4:
+    end_date = current_date + timedelta(weeks=-1, days=current_date.weekday() - 4)
+else:
+    end_date = current_date + timedelta(weeks=-2, days=current_date.weekday() + 3)
+
 
 actual_date = test_date
 
 while actual_date <= end_date:
-    year = actual_date.date().year
-    month = str(actual_date.date().month).rjust(2, "0")
-    day = str(actual_date.date().day).rjust(2, "0")
+    year = actual_date.year
+    month = str(actual_date.month).rjust(2, "0")
+    day = str(actual_date.day).rjust(2, "0")
     url = f"https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-{year}{month}{day}.csv"
 
     response = requests.get(url)
@@ -43,6 +47,6 @@ while actual_date <= end_date:
     else:
         print("Error for", year, month, day, "Skipping", "\n")
 
-    actual_date = actual_date.shift(days=1)
+    actual_date += timedelta(days=1)
 
     time.sleep(float(os.getenv("SLEEP_SECONDS")))
